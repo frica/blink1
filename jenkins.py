1bd703f51c0912b0990655f53cb5f23e93a88d2d
frica <fabien.rica@gmail.com>
2015-05-05 22:50:18 +0200

    working script with jenkins job from apache

--- jenkins.py
+++ jenkins.py
@@ -3,14 +3,30 @@
 from blink1.blink1 import Blink1
 import requests
 import time
+import json
 
 blink = Blink1()
 
-url = "https://jenkins.qa.ubuntu.com/api/python"
+# example job
+url = 'https://builds.apache.org/job/AuroraBot/lastBuild/api/json'
 
 try:
-    resp = requests.get(url, timeout=30).text
+    response = requests.get(url)
+    response.raise_for_status()
 except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
     blink.fade_to_color(500, 'white')
 
-print(resp)
+try:
+    jenkinsData = json.loads(response.text)
+except ValueError:
+    print("invalid json")
+else:
+    print(jenkinsData)
+    if "result" in jenkinsData:
+        print(jenkinsData["result"])
+        if jenkinsData["result"] == "SUCCESS":
+            blink.fade_to_color(2000, "blue")
+            time.sleep(2)
+
+blink.fade_to_rgb(1000, 0, 0, 0)
+blink.close()
