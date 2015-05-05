bc0356fa0d099d8f84b3be237c07cdcdd7c618b2
U-AGFAHEALTHCARE\awour <awour@MORWI429.agfahealthcare.com>
2015-05-05 16:36:02 +0200

    example with jenkins

---
+++ jenkins.py
@@ -0,0 +1,14 @@
+from blink1.blink1 import Blink1
+import requests
+import time
+
+blink = Blink1()
+
+url = "http://jenkins01-iibu.agfahealthcare.com/api/python"
+
+try:
+    resp = requests.get(url, timeout=30).text
+except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+    blink.fade_to_color(500, 'white')
+
+print(resp)
