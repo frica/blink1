aecf8cabb29339c7048c3e827579fe1454a95d31
frica <fabien.rica@gmail.com>
2015-05-14 17:21:08 +0200

    all patterns supported + doc

--- README.md
+++ README.md
@@ -6,14 +6,14 @@ My scripts using blink(1) USB RGB LED by ThingM http://blink1.thingm.com/
 
 # Use
 
-Simply run like this:
+Simply run (on Ubuntu 14.04):
 
 	python3 demo1.py
 
-**Note**
-
-* demo1.py needs you to compile from source the latest version of the python API, at least if you are using Ubuntu. 
+* demo1.py needs you to compile from source the latest version of the python API, at least if you are using Ubuntu.
+* starter.py blinks asd many times as my Github name is found on the clipboard
 * jenkins.py is inspired by https://github.com/msherry/blink-jenkins
+* patterns.py play the same sample patterns available in the BlinkControl tool
 
 **Note about BlinkControl**
 
@@ -23,18 +23,16 @@ First do:
 
 	sudo apt-get install libusb-1.0-0-dev
 
-Go to the folder commandline and 
+Go to the folder commandline and type:
 
 	make
 
-Then install:
+Then install the following packages:
 
 	sudo apt-get install libgl1-mesa-dev
 	sudo apt-get install mesa-common-dev
 
 Install QT 5.3 (you'll need to create an account http://www.qt.io/download-open-source/)
-
 Start QTCreator and open qt/blink1control/blink1control.pro
-
 Click Rebuild: here it is!
 
--- patterns.py
+++ patterns.py
@@ -15,29 +15,25 @@ def hex_to_rgb(value):
     return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
 
-def play_simple_pattern(pattern, blink):
-    """ play a simple pattern with 2 colors
-    Example: ['3', '#ff00ff', '0.5', '0', '#000000', '0.5', '']
+def play_pattern(pattern, blink):
+    """ play a pattern with multiple colors
+    Example: ['6', '#ff0000', '0.3', '1', '#0000ff', '0.3', '2', '#000000', '0.1', '0', '#ff0000', '0.3', '2', '#0000ff', '0.3', '1', '#000000', '0.1', '0']
     """
+    print(pattern)
     repeat = int(pattern[0])
-    first_color = pattern[1]
-    fade_first_color = float(pattern[2])
-    fade_first_color = int(fade_first_color * 1000)
-    print(fade_first_color)
-    led_fc = int(pattern[3])
-    sec_color = pattern[4]
-    fade_sec_color = int(float(pattern[5]) * 1000)
 
-    # test if last parameter is an empty string
-    led_sc = int(pattern[6]) if pattern[6] else 0
-    print("We will flash {0} times between {1} and {2}".format(repeat, first_color, sec_color))
-    for idx in range(0, repeat):
-        blink.fade_to_rgb(fade_first_color, hex_to_rgb(first_color)[0], hex_to_rgb(first_color)[1],
-                          hex_to_rgb(first_color)[2], led_fc)
-        time.sleep(1)
-        blink.fade_to_rgb(fade_sec_color, hex_to_rgb(sec_color)[0], hex_to_rgb(sec_color)[1], hex_to_rgb(sec_color)[2],
-                          led_sc)
-        time.sleep(1)
+    print("We will repeat {0} times this pattern.".format(repeat))
+    for loop in range(0, repeat):
+        index = 1
+        for i in range(0, len(pattern) // 3):
+            fade = int(float(pattern[index + 1]) * 1000)
+            led = int(pattern[index + 2]) if pattern[index + 2] else 0
+            if loop == 0:
+                print("Color {0} (Index {1}): {2} - LED {3} - Fade {4}".format(i+1, index, pattern[index], led, fade))
+            blink.fade_to_rgb(fade, hex_to_rgb(pattern[index])[0], hex_to_rgb(pattern[index])[1],
+                              hex_to_rgb(pattern[index])[2], led)
+            time.sleep(1)
+            index += 3
 
 
 blink = Blink1()
@@ -58,9 +54,7 @@ if len(patternData) != 0:
     for i in range(0, len(patternData)):
         pattern = [x.strip() for x in patternData[i]["pattern"].split(',')]
         print("Pattern: {}".format(patternData[i]["name"]))
-        if i < 6:
-            print("Simple pattern detected, let's play it!")
-            play_simple_pattern(pattern, blink)
+        play_pattern(pattern, blink)
 
 blink.fade_to_rgb(1000, 0, 0, 0)
 blink.close()
